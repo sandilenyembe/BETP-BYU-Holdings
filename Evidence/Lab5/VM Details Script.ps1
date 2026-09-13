980a479c13432cb84da288b6a134149278167f17
sandilenyembe <sandilenyembe@gmail.com>
2026-09-13 12:00:31 +0200

    Add files via upload

---
+++ Evidence/Lab5/VM Details Script.ps1
@@ -0,0 +1,11 @@
+﻿Get-VM | ForEach-Object {
+    [PSCustomObject]@{
+        VMName        = $_.Name
+        State         = $_.State
+        CPUCount      = $_.ProcessorCount
+        StartupMemory = $_.MemoryStartup / 1GB
+        AssignedMemory= $_.MemoryAssigned / 1GB
+        Uptime        = $_.Uptime
+        Host          = $env:COMPUTERNAME
+    }
+}
